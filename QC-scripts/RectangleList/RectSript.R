419db9c910b72941d0f24e7abf1db22f7e7af6e6
Molla Gazi <56016446+karolinamg@users.noreply.github.com>
2022-02-04 13:30:10 +0100

    upd rectangle

--- QC-scripts/RectangleList/RectSript.R
+++ QC-scripts/RectangleList/RectSript.R
@@ -2,7 +2,7 @@
 rm(list=(ls()))
 library(tidyverse)
 
-Irect <- read.csv("C:/Users/molla004/OneDrive - Wageningen University & Research/Git/hub/RDBES/QC-scripts/RectangleList/ICES_StatRec_mapto_ICES_Areas 2021.csv")
+Irect <- read.csv("./ICES_StatRec_mapto_ICES_Areas 2021.csv")
 
 
 Irect <- select(Irect, ICESNAME, Area_27, AreasList) %>%
@@ -20,8 +20,12 @@ new <- strsplit(Alist$AreasList, ",")
 
 # Create new df 
 IrectList <- data.frame(ICESNAME = rep(Alist$ICESNAME, sapply(new, length)), 
-                        Area_27 = rep(Alist$Area_27, sapply(new, length)), 
+                        Area_27 = rep(Alist$Area_27, sapply(new, length)),
                         AreasList = unlist(new))
+
+# # Add subarea
+# IrectList <- mutate(IrectList, Subarea = sub("\\..*", "", Area_27))
+# NoAlist  <- mutate(NoAlist , Subarea = sub("\\..*", "", Area_27))
 
 # Fix the space after the comma in the new df 
 IrectList <- mutate(IrectList,  AreasList = gsub(" ", "", AreasList))
@@ -33,27 +37,47 @@ Irectbind <- rbind(NoAlist, IrectList)
 Irectnew <- select(Irectbind, ICESNAME, AreasList) %>%
   distinct()
 
+Irectnew   <- mutate(Irectnew , Subarea = sub("\\..*", "", AreasList))
+
+
+subarea <- select(Irectnew, ICESNAME, Subarea) %>%
+  rename(AreasList = Subarea)
+
+Irectnew <- select(Irectnew, ICESNAME,AreasList)
+
+Irectnew1 <- rbind(Irectnew, subarea)
+
 unique(Irectnew$AreasList)
 
 
 # Account for most combinations of areas/rectangles (subarea, division, subdivision - NEAFC)
 
 # Get out divisions (ignore area 3 subdivisions)
-newdf <- mutate(Irectnew, Check = str_detect(AreasList, ".*\\..*\\..*"))
+newdf <- mutate(Irectnew1, Check = str_detect(AreasList, ".*\\..*\\..*"))
+
+
 newdf1 <- mutate(newdf, Division = ifelse(Check %in% TRUE & !str_detect(AreasList, "3."), sub("^([^.]*.[^.]*).*", "\\1", AreasList), AreasList))
 
 
 # Create final list 
 
+# & !str_detect(AreasList, "3.")
+
 ICESrectfin <- newdf1 %>% select(ICESNAME, AreasList, Division) %>% gather(source, Area_27, -ICESNAME) %>%
   select(Area_27, ICESNAME) %>%
   distinct()
+# Correct area 3.a (Add it)
+
+ll <- filter(ICESrectfin, str_detect(Area_27, "3.a")) %>%
+  mutate(Area_27 = "3.a",
+         Area = paste("27.",Area_27,sep=""))
 
 ICESrectfin$Area <- paste("27.",ICESrectfin$Area_27,sep="")
 
+ICESrectfin2 <- rbind(ICESrectfin, ll)
 
 # Write final df 
 
-write.csv(ICESrectfin,"C:/Users/molla004/OneDrive - Wageningen University & Research/Git/hub/RDBES/QC-scripts/RectangleList/ICESRectAreas.csv", row.names = FALSE)
+write.csv(ICESrectfin2,"./ICESRectAreas.csv", row.names = FALSE)
 
 
